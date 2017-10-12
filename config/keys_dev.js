ba970f6ee892ebb4d543590a96c75fca8ee3979b
Shravan Zolgikar <shravan.zolgikar@innoflexion.com>
2017-10-12 11:31:08 +0530

    disbaled devKeys

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,4 @@
 module.exports = {
- stripePublishableKey:'pk_test_Is7fxuhcd7vmcwNkXoQ514Ik',
-    stripeSecretKey:'sk_test_sLVtCUZ1HIzQ1Q1Itu6eur7Y'
+ stripePublishableKey:stripePublicKeys,
+    stripeSecretKey:stripeSecretKeys
 }
